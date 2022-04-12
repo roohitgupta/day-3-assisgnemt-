998206d935802748d92671e327360163824ece18
roohitgupta <roohitguptagit config [--global] user.email email@address.com>
2022-04-12 16:58:21 +0530

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Button from './Component/button'
+
+function App() {
+
+  const Value = 10
+
+  const [counter, setCounter] = useState(Value)
+  
+  
+  
+
+  const HandleChange = (Value)=>{
+    //condition
+    setCounter(counter + Value)
+  };
+
+  const DoubleValue = ()=>{
+   setCounter(counter * 2)
+  }
+  
+  const Reset = ()=>{
+    setCounter(Value)
+  }
+
+ 
+
+
+  return (
+    <div className="App">
+   
+    <h3 className={counter%2===0?"red":"blue"}>counter : {counter}</h3>
+    <Button onClick={()=>{HandleChange(-1)}} value="Subtract" />
+    <Button onClick={()=>{HandleChange(1)}} value="Adding" />
+    {/* <button onClick={()=>{HandleChange(-1)}}>Sub</button>
+    <button onClick={()=>{HandleChange(1)}}>Add</button> */}
+    <button onClick={()=>{DoubleValue()}}>Double</button>
+    <button onClick={()=>{Reset()}}>Reset</button>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Component/button.jsx
@@ -0,0 +1,9 @@
+
+
+const Button = (props)=>{
+    
+    return <button onClick={props.onClick}>{props.value}</button>;
+
+  };
+
+  export default Button;
